8f34dab25bf25b2d98eec84316d049487581b0d2
EVH <fbsehi@leeds.ac.uk>
2025-02-05 16:03:45 +0000

    Cleanup of unnecessary functions.

--- htvs_ad4gpu.py
+++ htvs_ad4gpu.py
@@ -119,91 +119,6 @@ def dock_single_ligand_ad4gpu(receptor_file, ligand):
 def dock_ligands_with_ad4gpu(receptor_file, ligands):
     return
 
-# Docking function for a single ligand using QuickVina2-GPU as a process call.
-def dock_single_ligand_qvina(receptor_file, docking_box, ligand):
-    idx, pdbqt_string = ligand
-    temp_ligand_path = None
-
-    try:
-        # vina_path = os.path.expanduser("~/mambaforge/envs/ringtail/bin/vina")
-        # if not os.path.exists(vina_path):
-        #     raise FileNotFoundError(f"Vina executable not found at {vina_path}")
-            
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdbqt") as temp_ligand_file:
-            temp_ligand_file.write(pdbqt_string.encode())  # Write ligand content to temp file.
-            temp_ligand_path = temp_ligand_file.name  # Save temp file path
-
-        vina_cmd = f'{qvina_executable} --receptor {receptor_file} --ligand {temp_ligand_path} --thread {qvina_gpu_threads} --center_x {docking_box["center"][0]} --center_y {docking_box["center"][1]} --center_z {docking_box["center"][2]} --size_x {docking_box["size"][0]} --size_y {docking_box["size"][1]} --size_z {docking_box["size"][2]} --opencl_binary_path {opencl_binary_path} --out /dev/stdout'
-        # Local testing with regular Vina.
-        # vina_cmd = f'{vina_path} --receptor {receptor_file} --ligand {temp_ligand_path} --center_x {docking_box["center"][0]} --center_y {docking_box["center"][1]} --center_z {docking_box["center"][2]} --size_x {docking_box["box_size"][0]} --size_y {docking_box["box_size"][1]} --size_z {docking_box["box_size"][2]} --out /dev/stdout'
-
-        # Run QuickVina2-GPU
-        result = subprocess.check_output(vina_cmd, text=True, shell=True, stderr=subprocess.STDOUT)
-        filtered_output = re.findall(r'(?s)(MODEL \d+.*?ENDMDL)', result)
-        # Join multiple poses into single string to match regular Vina format
-        combined_poses = '\n'.join(filtered_output) if filtered_output else None
-        return idx, combined_poses
-    except subprocess.CalledProcessError as e:
-        print(f"Vina docking failed for {idx}: {e.output}")
-        return idx, None
-    except Exception as e:
-        print(f"Error in docking {idx}: {str(e)}")
-        return idx, None
-    finally:
-        if temp_ligand_path and os.path.exists(temp_ligand_path):
-            os.remove(temp_ligand_path)
-
-# Docking function using QuickVina2-GPU with joblib parallelization
-def dock_ligands_with_qvina(receptor_file, docking_box, ligands):
-    # Use joblib to parallelize the docking
-    results = Parallel(n_jobs=n_cores_vina)(
-        delayed(dock_single_ligand_qvina)(receptor_file, docking_box, ligand)
-        for ligand in ligands
-    )
-
-    # Convert results to dictionary. Ligand variant names are appended by a number for now.
-    """TODO: Building of this results dict is broken due to a key collision issue with same ligand names that are used as the dictionary key.
-    At the same time, Ringtail needs the dictionary to be in this format for result parsing.
-    """
-    vina_output = {}
-    for idx, poses in results:
-        if poses is not None:
-            vina_output[idx] = poses
-    return vina_output # NOTE: There is a discrepancy between this and the regular vina docking function output format which is causing the Ringtail parse error.
-
-# Docking function for a single ligand using Vina
-def dock_single_ligand(receptor_file, docking_box, ligand):
-    idx, pdbqt_string = ligand
-    try:
-        v = Vina(sf_name = vina_scoring_function, cpu = n_vina_threads, verbosity = 0)
-        v.set_receptor(receptor_file)
-        v.set_ligand_from_string(pdbqt_string)
-        v.compute_vina_maps(**docking_box)
-        v.dock(exhaustiveness=8, n_poses=5)
-        vina_poses = v.poses()
-        return idx, vina_poses
-    except Exception as e:
-        print(f"Error docking ligand {idx}: {e}")
-        return idx, None
-
-# Docking function using Vina with joblib parallelization
-def dock_ligands_with_vina(receptor_file, docking_box, ligands):
-    # Use joblib to parallelize the docking
-    results = Parallel(n_jobs=n_cores_vina)(
-        delayed(dock_single_ligand)(receptor_file, docking_box, ligand)
-        for ligand in ligands
-    )
-
-    # Convert results to dictionary. Ligand variant names are appended by a number for now.
-    """TODO: Building of this results dict is broken due to a key collision issue with same ligand names that are used as the dictionary key.
-    At the same time, Ringtail needs the dictionary to be in this format for result parsing.
-    """
-    vina_output = {}
-    for idx, poses in results:
-        if poses is not None:
-            vina_output[idx] = poses
-    return vina_output
-
 # Function to handle batching of ligands.
 def process_batches(ligand_input_file, batch_size):
     start_time = time.time()
@@ -274,7 +189,7 @@ def process_batches(ligand_input_file, batch_size):
         print(f"Generated {len(flattened_batch)} variants for {len(batch)} ligands in final batch ({prep_time:.2f}s)")
 
         dock_start = time.time()
-        vina_results = dock_ligands_with_qvina(receptor_file, docking_box, flattened_batch) # Docking program spec
+        vina_results = dock_ligands_with_ad4gpu(receptor_file, flattened_batch) # Docking program spec
         dock_time = time.time() - dock_start
         print(f"Successfully docked {len(vina_results)} variants in final batch ({dock_time:.2f}s)")
 
